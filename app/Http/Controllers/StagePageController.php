a896d6123e15e7ce7f60c1bbae9d24c4f396a696
luca <luca.carnaroli98@gmail.com>
2020-03-16 17:18:16 +0100

    controller

---
+++ app/Http/Controllers/StagePageController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StagePageController extends Controller
+{
+    public function album()
+    {
+        $cds = [
+            [
+                'post' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+                'song' => 'New Jersey',
+                'artist' => 'Bon Jovi',
+                'year' => '1988',
+            ],
+
+            [
+                'post' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+                'song' => 'Live at Wembley 86',
+                'artist' => 'Queen',
+                'year' => '1992',
+            ],
+
+            [
+                'post' =>  
+                'song' => 
+                'artist' =>
+                'year' =>
+            ],
+
+            [
+                'post' =>  
+                'song' => 
+                'artist' =>
+                'year' =>
+            ],
+
+            [
+                'post' =>  
+                'song' => 
+                'artist' =>
+                'year' =>
+            ],
+
+            [
+                'post' =>  
+                'song' => 
+                'artist' =>
+                'year' =>
+            ],
+
+        ]
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/album', 'StaticPageController@album')->name('album');
 
 Route::get('/', function () {
     return view('cds');
