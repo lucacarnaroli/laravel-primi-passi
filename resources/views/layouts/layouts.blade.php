6899e9a4be3aa26e4690566e19d8a3f0aea11711
luca <luca.carnaroli98@gmail.com>
2020-03-16 23:37:06 +0100

    yield

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -1,19 +1,17 @@
 @include('partials._header')
 {{-- inludo con blade(@) e aggiungo percorso delle cartelle con la dot notation --}}
+<body>
+    <div class="container">
+        <header>
+            <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
+        </header>
+        <div class="cds-container">
+            @yield('header')
+            {{-- yield è come un segnaposto --}}
+        </div>
+        </div>
 
-<div>
-    <header>
-        @yield('header')
-        {{-- yield è come un segnaposto --}}
-    </header>
-
-    <main>
-        @yield('content')
-    </main>
-
-    <footer>
-        @yield('footer')
-    </footer>
-</div>
+</body>
+</html>
 
 @include('partials._footer')
